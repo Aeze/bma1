46e5248449572f770d6f9eaa443dbc564177988a
Marcus Orochena <marcus.orochena@gmail.com>
2013-06-30 20:41:49 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ctsm::Application.config.secret_key_base = '92b427bcbae624aff8c2cd7339fbe9b50eadf09b978d7bb3f392e84aaa70b40d7cae01e7c95f0f2827108ed016954556d0308eec9874c2d587867b1cc3fa2c1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ctsm::Application.config.session_store :cookie_store, key: '_ctsm_session'
