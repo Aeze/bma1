edc92ba5b6afc31918e35a8cb4e9f4199d54823b
Marcus Orochena <marcus.orochena@gmail.com>
2013-07-06 23:14:13 -0400

    contact page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Ctsm::Application.routes.draw do
 
     resources :publications
 
+    get 'pages/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
